af94dcaac241bd98dcae63db82c4f2d29ee57e86
Phyrria <lele.ho05@gmail.com>
2025-12-14 19:20:42 +0100

    Add cachix module

---
+++ nixos/modules/cachix.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = [ pkgs.cachix ];
+
+  nix.settings = {
+    substituters = [ "https://ezkea.cachix.org" ];
+    trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
+  };
+}
